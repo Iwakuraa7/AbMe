2ad7c50f7a2dca7dd7bea69b65dda95926ff4927
Erlan_Esengeldiev <ee12407@auca.kg>
2024-10-15 18:27:42 +0600

    Add sign in mockup

--- AbMe-frontend/react-frontend/pages/SignInPage.jsx
+++ AbMe-frontend/react-frontend/pages/SignInPage.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 export default function SingInPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function signInUser(e) {
+        e.preventDefault();
+        console.log(username, password);
+    }
+
     return(
         <>
-        <h1>SignIn Page</h1>
+        <form onSubmit={signInUser}>
+        <div class='authCard'>
+            <div class='authMessageBox'>
+                <h2>Welcome back</h2>
+            </div>
+            <div class='inputFieldsBox'>
+                <div>
+                    <label for='usernameField'>Username</label><br/>
+                    <input onChange={(e) => {setUsername(e.target.value)}} id='usernameField' type='text'/>
+                </div>
+
+                <div>
+                    <label for='passwordField'>Password</label><br/>
+                    <input onChange={(e) => {setPassword(e.target.value)}} id='passwordField' type='password'/>
+                </div>
+            </div>
+            <button type='submit'>Sign in</button>
+        </div>
+        </form>
         </>
     )
 }
