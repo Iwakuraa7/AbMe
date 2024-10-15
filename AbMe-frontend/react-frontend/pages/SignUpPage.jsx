b3ab6797ea0100a8195bb10b0dfe8edc7a575bb7
Erlan_Esengeldiev <ee12407@auca.kg>
2024-10-15 18:25:32 +0600

    Add sign up page mockup

--- AbMe-frontend/react-frontend/pages/SignUpPage.jsx
+++ AbMe-frontend/react-frontend/pages/SignUpPage.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react"
+
 export default function SignUpPage() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function registerNewUser(e) {
+        e.preventDefault();
+        console.log(username, email, password);
+    }
+
     return(
         <>
-        <h1>SignUp Page</h1>
+        <form onSubmit={registerNewUser}>
+        <div class='authCard'>
+            <div class='authMessageBox'>
+                <h2>Welcome to Abme</h2>
+            </div>
+            <div class='inputFieldsBox'>
+                <div>
+                    <label for='usernameField'>Username</label><br/>
+                    <input onChange={(e) => {setUsername(e.target.value)}} id='usernameField' type='text'/>
+                </div>
+
+                <div>
+                    <label for='emailField'>Email</label><br/>
+                    <input onChange={(e) => {setEmail(e.target.value)}} id='emailField' type='email'/>
+                </div>
+
+                <div>
+                    <label for='passwordField'>Password</label><br/>
+                    <input onChange={(e) => {setPassword(e.target.value)}} id='passwordField' type='password'/>
+                </div>
+            </div>
+            <button type='submit'>Create new account</button>
+        </div>
+        </form>
         </>
     )
 }
