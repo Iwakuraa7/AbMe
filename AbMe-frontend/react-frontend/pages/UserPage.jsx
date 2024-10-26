45ed7519988fb0d687b6d475be128c5499e6591d
Erlan_Esengeldiev <ee12407@auca.kg>
2024-10-26 21:59:34 +0600

    Add Delete button to user's music profile's entitites

--- AbMe-frontend/react-frontend/pages/UserPage.jsx
+++ AbMe-frontend/react-frontend/pages/UserPage.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
+import {jwtDecode } from "jwt-decode";
 
 export default function UserPage() {
+    const [isOwner, setIsOwner] = useState(null);
     const [musicData, setMusicData] = useState(null);
     const [expandedHobby, setExpandedHobby] = useState(null);
     const params = useParams();
@@ -20,26 +22,61 @@ export default function UserPage() {
 
             if(data.succeeded) {
                 setMusicData(data.musicData);
-                console.log("+ userMusicData");
             }
             else {
                 console.log("sth went wrong while gettin userMusicData...");
             }
         }
 
+        function checkOwnership() {
+            var token = localStorage.getItem('token');
+            var userInfo = jwtDecode(token);
+            console.log(userInfo);
+
+            if(userInfo.given_name === params.username) {
+                setIsOwner(true);
+            }
+        }
+
         fetchMusicData();
+        checkOwnership();
     }, [])
+
+    async function deleteMusicData(musicId) {
+        var response = await fetch(`http://localhost:5078/api/music/delete/${musicId}`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem('token'),
+                "Content-Type": "application/json"
+            }
+        })
+
+        var data = await response.json();
+
+        if(data.succeeded) {
+            musicData.splice(musicId, musicId);
+            console.log(data.message);
+        }
+        else {
+            console.log("sth went wrong during deletion....");
+        }
+    }
 
     const renderMusicContent = () => {
         return(
         <div>
             <h2>{params.username} music taste</h2>
+            <button onClick={() => setExpandedHobby(null)}>Back</button>
             {musicData.map(music => (
+                <div>
                 <img key={music.id} src={music.imageUrl} alt={`Music photo ${music.id + 1}`}/>
+                {isOwner && (
+                    <button onClick={() =>deleteMusicData(music.id)}>Delete</button>
+                )}
+                </div>
             ))}
         </div>)
     }
-
 
     return (
         <>
