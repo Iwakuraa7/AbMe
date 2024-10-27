4ccd6dc1e19beede9acdc215272aa3ab2a75e3f3
Erlan_Esengeldiev <ee12407@auca.kg>
2024-10-27 12:55:25 +0600

    Add routes

--- AbMe-frontend/react-frontend/pages/MainPage.jsx
+++ AbMe-frontend/react-frontend/pages/MainPage.jsx
@@ -1,19 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import LogoutButton from "../components/LogoutButton";
 import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 export default function MainPage() {
+    const [userInfo, setUserInfo] = useState(null);
     const navigate = useNavigate();
 
     useEffect(() => {
         if(localStorage.getItem('token') === null) {
             return navigate('/');
         }
-    })
+
+        var decodedToken = jwtDecode(localStorage.getItem('token'));
+        setUserInfo(decodedToken);
+    }, [])
 
     return(
         <>
         <h1>Main Page</h1>
+        <button onClick={() => {navigate(`/user/${userInfo.given_name}`)}}>My profile</button>
+        <button onClick={() => navigate('/search/music')}>Search music</button>
+        <LogoutButton/>
         </>
     )
 }
