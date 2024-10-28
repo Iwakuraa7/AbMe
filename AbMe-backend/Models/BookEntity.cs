e8d70c29115055fd64e09ab2f93bf8f495528271
Erlan_Esengeldiev <ee12407@auca.kg>
2024-10-28 19:21:15 +0600

    Create BookEntity

---
+++ AbMe-backend/Models/BookEntity.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbMe_backend.Models
+{
+    public class BookEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ImageUrl { get; set; }
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }        
+    }
+}
