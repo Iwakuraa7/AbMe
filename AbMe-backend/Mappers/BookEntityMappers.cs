04ea022f35a2b83586dfc058ba00dc35691905d4
Erlan_Esengeldiev <ee12407@auca.kg>
2024-10-28 19:23:14 +0600

    Create book entity mappers

---
+++ AbMe-backend/Mappers/BookEntityMappers.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AbMe_backend.Dtos.BookEntity;
+using AbMe_backend.Models;
+
+namespace AbMe_backend.Mappers
+{
+    public static class BookEntityMappers
+    {
+        public static BookEntity fromCreateDtoToModel(this CreateBookEntityDto bookDto)
+        {
+            return new BookEntity
+            {
+                Title = bookDto.Title,
+                Author = bookDto.Author,
+                ImageUrl = bookDto.ImageUrl
+            };
+        }
+
+        public static BookEntityDto fromModelToDto(this BookEntity bookModel)
+        {
+            return new BookEntityDto
+            {
+                Id = bookModel.Id,
+                Title = bookModel.Title,
+                Author = bookModel.Author,
+                ImageUrl = bookModel.ImageUrl,
+                AppUserId = bookModel.AppUserId
+            };
+        }
+    }
+}
