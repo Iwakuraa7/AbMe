8e3b5cb2b446feffbaffb1a1448bd911de9acc9d
Erlan_Esengeldiev <ee12407@auca.kg>
2024-10-30 22:34:37 +0600

    Adjust rendering logic for signIn/Up and About info

--- AbMe-frontend/react-frontend/pages/HomePage.jsx
+++ AbMe-frontend/react-frontend/pages/HomePage.jsx
@@ -1,10 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./HomePage.module.css"
+import SingInPage from "./SignInPage";
+import SignUpPage from "./SignUpPage";
+import AboutInfo from "../components/AboutInfo";
 
 
 export default function HomePage() {
     const navigate = useNavigate();
+    const [isSignIn, setIsSignIn] = useState(false);
+    const [isSignUp, setIsSignUp] = useState(false);
+    const [isAbout, setIsAbout] = useState(false);
 
     useEffect(() => {
         if(localStorage.getItem('token') !== null) {
@@ -17,19 +23,25 @@ export default function HomePage() {
         <div className={styles["main-grid"]}>
             <div className={styles["left-grid-component"]}/>
 
-            <div className={styles["middle-grid-component"]}>
+            {!isSignIn && !isSignUp && (<div className={styles["middle-grid-component"]}>
                 <div className={styles["up-middle-grid-component"]}/>
 
                 <div className={styles["down-middle-grid-component"]}/>
-            </div>
+            </div>)}
 
             <div className={styles["right-grid-component"]}>
-                <div className={styles["up-right-grid-component"]}/>
+                <div className={styles["up-right-grid-component"]} onClick={() => {setIsAbout(!isAbout)}}/>
 
-                <div className={styles["middle-right-grid-component"]} onClick={() => navigate('/signin')}/>
+                <div className={styles["middle-right-grid-component"]} onClick={() => {setIsSignIn(!isSignIn); setIsSignUp(false)} }/>
 
-                <div className={styles["down-right-grid-component"]} onClick={() => navigate('/signup')}/>
+                <div className={styles["down-right-grid-component"]} onClick={() => {setIsSignUp(!isSignUp); setIsSignIn(false)}}/>
             </div>
+            
+            {isSignIn && (<SingInPage/>)}
+
+            {isSignUp && (<SignUpPage/>)}
+
+            {isAbout && (<AboutInfo/>)}
         </div>
         </>
     )
