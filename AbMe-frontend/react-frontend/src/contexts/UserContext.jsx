331845e02f4277c880ec2180b050a0b13a5a0d68
Erlan_Esengeldiev <ee12407@auca.kg>
2024-11-01 12:05:36 +0600

    Fix bug with token

--- AbMe-frontend/react-frontend/src/contexts/UserContext.jsx
+++ AbMe-frontend/react-frontend/src/contexts/UserContext.jsx
@@ -1,10 +1,19 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 
 export const UserContext = createContext();
 
 export function UserProvider({ children }) {
-    const [userInfo, setUserInfo] = useState(jwtDecode(localStorage.getItem('token')))
+    const [userInfo, setUserInfo] = useState(null);
+
+    useEffect(() => {
+        var token = localStorage.getItem('token');
+        if(token === null)
+            return;
+
+        var decodedToken = jwtDecode(token);
+        setUserInfo(decodedToken);
+    }, [localStorage.getItem('token')])
 
     return (
         <UserContext.Provider value={{userInfo, setUserInfo}}>
