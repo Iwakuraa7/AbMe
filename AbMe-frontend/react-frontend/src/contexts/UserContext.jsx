e37ccb4aaa17502d01fc3c5b41089d52fd63adbb
Erlan_Esengeldiev <ee12407@auca.kg>
2024-11-01 20:50:22 +0600

    Remove JWT logic

--- AbMe-frontend/react-frontend/src/contexts/UserContext.jsx
+++ AbMe-frontend/react-frontend/src/contexts/UserContext.jsx
@@ -6,15 +6,6 @@ export const UserContext = createContext();
 export function UserProvider({ children }) {
     const [userInfo, setUserInfo] = useState(null);
 
-    useEffect(() => {
-        var token = localStorage.getItem('token');
-        if(token === null)
-            return;
-
-        var decodedToken = jwtDecode(token);
-        setUserInfo(decodedToken);
-    }, [localStorage.getItem('token')])
-
     return (
         <UserContext.Provider value={{userInfo, setUserInfo}}>
             { children }
