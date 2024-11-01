04693bf27a260f149df1bc3b9377537b5da1116b
Erlan_Esengeldiev <ee12407@auca.kg>
2024-11-01 20:51:07 +0600

    Add search user feature

--- AbMe-frontend/react-frontend/pages/MainPage.jsx
+++ AbMe-frontend/react-frontend/pages/MainPage.jsx
@@ -3,9 +3,12 @@ import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import NavBar from "../components/NavBar";
 import styles from "../styles/MainPage.module.css"
+import searchStyles from "../styles/SearchPage.module.css"
 
 export default function MainPage() {
     const [userInfo, setUserInfo] = useState(null);
+    const [userSearchQuery, setUserSearchQuery] = useState(null);
+    const [userSearchResult, setUserSearchResult] = useState(null);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -17,6 +20,16 @@ export default function MainPage() {
         setUserInfo(decodedToken);
     }, [])
 
+    async function handleUserSearch() {
+        var response = await fetch(`http://localhost:5078/api/account/user-search/${userSearchQuery}`);
+        var data = await response.json();
+
+        if(data.succeeded)
+            setUserSearchResult(data.usernames);
+        else
+            console.error("Sth went wrong while finding usernames by the value...")
+    }
+
     return(
         <>
         <NavBar/>
@@ -26,8 +39,22 @@ export default function MainPage() {
             </div>
             <div className={styles["search-user-input-box"]}>
                 <label>Searching for someone?</label>
-                <input type="text"/>
+                <div className={searchStyles["search-and-button-rel"]}>
+                    <input onChange={(e) => setUserSearchQuery(e.target.value)} type="text"/>
+                    <button className={styles["button-style"]} onClick={() => handleUserSearch()}>Find</button>
+                </div>
             </div>
+
+            {userSearchResult &&
+            (<div className={styles["usernames-box"]}>
+                {userSearchResult.map(u => {
+                    return (
+                        <div className={styles["usernameEntity-box"]} onClick={() => navigate(`/user/${u}`)}>
+                            <h3>{u}</h3>
+                        </div>
+                    )
+                })}
+            </div>)}
         </div>
         </>
     )
