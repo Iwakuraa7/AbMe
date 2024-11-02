83ead80251b7bea9a6cd4554a23aa6d3bb0ac32a
Erlan_Esengeldiev <ee12407@auca.kg>
2024-11-02 18:41:03 +0600

    Create Manga Model

---
+++ AbMe-backend/Models/MangaEntity.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbMe_backend.Models
+{
+    public class MangaEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }          
+    }
+}
