0acd3b5e6772a05d0b442e5f42728895c4c7f770
Erlan_Esengeldiev <ee12407@auca.kg>
2024-11-02 18:44:01 +0600

    Create DTOs from AnimeEntity

---
+++ AbMe-backend/Dtos/AnimeEntity/AnimeEntityDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbMe_backend.Dtos.AnimeEntity
+{
+    public class AnimeEntityDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }        
+    }
+}
