2835283ddb57622287ec718e0ec49d5cf40856ec
Erlan_Esengeldiev <ee12407@auca.kg>
2024-11-02 18:40:09 +0600

    Add Anime and Manga Models

---
+++ AbMe-backend/Migrations/20241102114819_Add Anime and Manga Models.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AbMe_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAnimeandMangaModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "22129590-d5cb-4fc3-b963-96efcba45656");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a4f8a2d6-ee7e-43d7-b3c8-db4f5cfde17a");
+
+            migrationBuilder.CreateTable(
+                name: "AnimeEntities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AppUserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnimeEntities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AnimeEntities_AspNetUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MangaEntities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AppUserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MangaEntities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MangaEntities_AspNetUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "c5094903-0467-46ed-810d-b70669cb4ff6", null, "User", "USER" },
+                    { "f5f19769-8de4-446b-a459-337ef4c2f3f2", null, "Admin", "ADMIN" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnimeEntities_AppUserId",
+                table: "AnimeEntities",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MangaEntities_AppUserId",
+                table: "MangaEntities",
+                column: "AppUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnimeEntities");
+
+            migrationBuilder.DropTable(
+                name: "MangaEntities");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c5094903-0467-46ed-810d-b70669cb4ff6");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f5f19769-8de4-446b-a459-337ef4c2f3f2");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "22129590-d5cb-4fc3-b963-96efcba45656", null, "Admin", "ADMIN" },
+                    { "a4f8a2d6-ee7e-43d7-b3c8-db4f5cfde17a", null, "User", "USER" }
+                });
+        }
+    }
+}
