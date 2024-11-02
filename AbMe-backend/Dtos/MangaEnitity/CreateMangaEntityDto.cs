ca17fb2d1d585abc2b7ffa9a54718f9700c9a2bf
Erlan_Esengeldiev <ee12407@auca.kg>
2024-11-02 18:44:29 +0600

    Create DTOs for MangaEntity

---
+++ AbMe-backend/Dtos/MangaEnitity/CreateMangaEntityDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbMe_backend.Dtos.MangaEnitity
+{
+    public class CreateMangaEntityDto
+    {
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }        
+    }
+}
