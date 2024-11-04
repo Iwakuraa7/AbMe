e517d1e13a2cc0df750d3c32433c2cfbff372b74
Erlan_Esengeldiev <ee12407@auca.kg>
2024-11-04 19:43:46 +0600

    Adjust rendering logic for easier integration of new features & for styles

--- AbMe-frontend/react-frontend/pages/UserPage.jsx
+++ AbMe-frontend/react-frontend/pages/UserPage.jsx
@@ -2,16 +2,19 @@ import { useEffect, useRef, useState } from "react"
 import { useParams } from "react-router-dom";
 import {jwtDecode } from "jwt-decode";
 import styles from "../styles/UserPage.module.css"
+import mainPageStyles from "../styles/MainPage.module.css"
 import NavBar from "../components/NavBar";
 
 export default function UserPage() {
     const [isOwner, setIsOwner] = useState(null);
     const [musicData, setMusicData] = useState(null);
     const [booksData, setBooksData] = useState(null);
+    const [animeData, setAnimeData] = useState(null);
+    const [mangaData, setMangaData] = useState(null);
+    const [randomDimensions, setRandomDimensions] = useState({});
     const [expandedHobby, setExpandedHobby] = useState(null);
     const [dataToDelete, setDataToDelete] = useState(null);
     const params = useParams();
-    const hobbyRollBoxRef = useRef(null);
 
     useEffect(() => {
         async function fetchHobbyData() {
@@ -30,7 +33,9 @@ export default function UserPage() {
                 if(data.succeeded) {
                     setMusicData(data.musicData);
                     setBooksData(data.booksData);
-                    console.log(data.musicData);
+                    setAnimeData(data.animeData);
+                    setMangaData(data.mangaData);
+                    console.log(data);
                 }
 
                 else
@@ -57,47 +62,40 @@ export default function UserPage() {
         checkOwnership();
     }, [params.username])
 
-    // useEffect(() => {
-    //     let scrollAmount = 0;
-    //     let speed = 1.5; // Adjust the speed of scrolling
-
-    //     function autoScroll() {
-    //         const box = hobbyRollBoxRef.current;
-
-    //         if (box) {
-    //             scrollAmount += speed;
-    //             if (scrollAmount >= box.scrollWidth - box.clientWidth) {
-    //                 scrollAmount = 0; // Reset scroll to loop
-    //             }
-    //             box.scrollLeft = scrollAmount;
-    //         }
-
-    //         requestAnimationFrame(autoScroll); // Smooth looping
-    //     }
-
-    //     autoScroll();
-    // }, [])
-
-    async function deleteMusicData(musicId) {
+    async function deleteRelevantData(dataId) {
         try
         {
-            var response = await fetch(`http://localhost:5078/api/music/delete/${musicId}`, {
+            var response = await fetch(`http://localhost:5078/api/${expandedHobby}/delete/${dataId}`, {
                 method: "DELETE",
                 headers: {
                     "Authorization": "Bearer " + localStorage.getItem('token'),
                     "Content-Type": "application/json"
-                }
+                }                
             })
 
             var data = await response.json();
 
             if(data.succeeded)
             {
-                setMusicData((prevData) => prevData.filter(m => m.id !== musicId));
+                switch(expandedHobby)
+                {
+                    case 'music':
+                        setMusicData((prevData) => prevData.filter(m => m.id !== dataId));
+                    case 'book':
+                        setBooksData((prevData) => prevData.filter(b => b.id !== dataId));
+                    case 'anime':
+                        setAnimeData((prevData) => prevData.filter(a => a.id !== dataId));
+                    case 'manga':
+                        setMangaData((prevData) => prevData.filter(m => m.id !== dataId));
+                    default:
+                        return;
+                }
                 console.log(data.message);
             }
             else
+            {
                 console.error("sth went wrong during deletion....");
+            }
         }
         catch(err)
         {
@@ -105,92 +103,80 @@ export default function UserPage() {
         }
     }
 
-    async function deleteBookData(bookId) {
-        try
+    function findRelevantData() {
+        switch(expandedHobby)
         {
-            var response = await fetch(`http://localhost:5078/api/book/delete/${bookId}`, {
-                method: "DELETE",
-                headers: {
-                    "Authorization": "Bearer " + localStorage.getItem('token'),
-                    "Content-Type": "application/json"
-                }
-            })
-
-            var data = await response.json();
-
-            if(data.succeeded)
-            {
-                setBooksData((prevData) => prevData.filter(m => m.id !== bookId));
-                console.log(data.message);
-            }
-            else
-                console.error("sth went wrong during deletion....");
-        }
-        catch(err)
-        {
-            console.error(err);
-        }
-    }
-
-    const renderMusicContent = () => {
-        return(
-        <div>
-            <h2>{params.username} music taste</h2>
-            <button onClick={() => setExpandedHobby(null)}>Back</button>
-            <div className="hobbyRollBox" ref={hobbyRollBoxRef}>
-            {musicData.map(music => (
-                <div
-                key={music.id}
-                className="searchResultEntity"
-                onMouseEnter={() => setDataToDelete(music.id)}
-                onMouseLeave={() => setDataToDelete(null)}>
-
-                <img key={music.id} src={music.imageUrl} alt={`Music photo ${music.id + 1}`}/>
-                <h2>{music.title}</h2>
-                {isOwner && dataToDelete === music.id && (
-                    <button onClick={() => deleteMusicData(dataToDelete)}>Delete</button>
-                )}
-                </div>
-            ))}
-            </div>
-        </div>)
-    }
-
-    const renderBooksContent = () => {
-        return(
-        <div>
-            <h2>{params.username} literature taste</h2>
-            <button onClick={() => setExpandedHobby(null)}>Back</button>
-            <div className="hobbyRollBox" ref={hobbyRollBoxRef}>
-            {booksData.map(book => (
-                <div
-                key={book.id}
-                className="searchResultEntity"
-                onMouseEnter={() => setDataToDelete(book.id)}
-                onMouseLeave={() => setDataToDelete(null)}
-                >
-
-                <img key={book.id} src={book.imageUrl} alt={`Music photo ${book.id + 1}`}/>
-                <h2>{book.title}</h2>
-                {isOwner && dataToDelete === book.id && (
-                    <button onClick={() => deleteBookData(dataToDelete)}>Delete</button>
-                )}
-                </div>
-            ))}
-            </div>
-        </div>)
-    }
-
-    const renderContent = () => {
-        switch(expandedHobby) {
             case 'music':
-                return renderMusicContent();
-            case 'books':
-                return renderBooksContent();
+                return musicData;
+            case 'book':
+                return booksData;
+            case 'anime':
+                return animeData;
+            case 'manga':
+                return mangaData;
             default:
                 return null;
-        }
+        }        
     }
+
+    useEffect(() => {
+        function createRandomDimensions() {
+            const dimensions = {};
+            findRelevantData().forEach(data => {
+                const randomWidth = expandedHobby === 'book' ? Math.floor(Math.random() * 100 + 100) : Math.floor(Math.random() * 100 + 200);
+                const randomHeight = expandedHobby === 'music' ? randomWidth : randomWidth * 1.5;
+                dimensions[data.id] = { width: randomWidth, height: randomHeight };
+            });
+            setRandomDimensions(dimensions);
+        }
+
+        if(expandedHobby !== null)
+            createRandomDimensions();
+    }, [expandedHobby])
+
+    const renderRelevantContent = () => {
+        var relevantData = findRelevantData();
+    
+        return (
+            <div>
+                <div className={styles["center-upper-elements"]}>
+                    <h2>{params.username}'s {expandedHobby} taste</h2>
+                    <button className={mainPageStyles["button-style"]} onClick={() => setExpandedHobby(null)}>Back</button>
+                </div>
+
+                <div className={styles["random-box"]}>
+                    {relevantData.map(data => (
+                        <div
+                            key={data.id}
+                            style={{
+                                maxWidth: randomDimensions[data.id]?.width,
+                                maxHeight: randomDimensions[data.id]?.height,
+                                margin: randomDimensions[data.id]?.width - (expandedHobby === 'book' ? 100 : 200),
+                            }}
+                            className={styles["expanded-hobby-entity-box"]}
+                            onMouseEnter={() => setDataToDelete(data.id)}
+                            onMouseLeave={() => setDataToDelete(null)}
+                        >
+                            <img className={styles["hobby-image"]} src={data.imageUrl} alt={`Data photo ${data.id + 1}`} />
+                            <h2 style={{ textAlign: "center", fontSize: "1rem", margin: "5px 0" }}>{data.title}</h2>
+                            {isOwner && (
+                                <button
+                                style={{display: dataToDelete === data.id ? "block" : "none"}}
+                                onClick={() => deleteRelevantData(dataToDelete)}
+                                className={styles["button-style"]}
+                                >
+                                    Delete
+                                </button>
+                            )}
+                        </div>
+                    ))}
+                </div>
+
+            </div>
+        );
+    };
+    
+    
 
     return (
         <>
@@ -198,39 +184,68 @@ export default function UserPage() {
         ?
         <>
         <NavBar/>
-        {renderContent()}
+        {renderRelevantContent()}
         </>
         :
         (
         <>
         <NavBar/>
-        <div>
-            <h2>{params.username}</h2>
-            <div onClick={() => setExpandedHobby('music')} className="userHobbyBox">
-                <div className="userHobbyBoxImages">
+        <h2 className={styles["center-text"]}>{params.username}</h2>
+        
+        <div className={styles["user-hobby-main-box"]}>
+
+            <div onClick={() => setExpandedHobby('music')} className={styles["userHobbyBox"]}>
+                <div className={styles["userHobbyBoxImages"]}>
                     {musicData && (
                         musicData.slice(0, 4).map(music => (
                             <img key={music.id} src={music.imageUrl} alt={`Music photo ${music.id + 1}`}/>
                         ))
                     )}
                 </div>
-                <div className="userHobbyBoxTitle">
+                <div className={styles["userHobbyBoxTitle"]}>
                     <strong>Music</strong>
                 </div>
             </div>
 
-            <div onClick={() => setExpandedHobby('books')} className="userHobbyBox bookImagesRes">
-                <div className="userHobbyBoxImages">
+            <div onClick={() => setExpandedHobby('book')} className={`${styles["userHobbyBox"]} ${styles["bookImagesRes"]}`}>
+                <div className={styles["userHobbyBoxImages"]}>
                     {booksData && (
                         booksData.slice(0, 8).map(book => (
                             <img key={book.id} src={book.imageUrl} alt={`Book photo ${book.id + 1}`}/>
                         ))
                     )}
                 </div>
-                <div className="userHobbyBoxTitle">
+                <div className={styles["userHobbyBoxTitle"]}>
                     <strong>Literature</strong>
                 </div>
             </div>
+
+            <div onClick={() => setExpandedHobby('anime')} className={`${styles["userHobbyBox"]} ${styles["bookImagesRes"]}`}>
+                <div className={styles["userHobbyBoxImages"]}>
+                    {animeData && (
+                        animeData.slice(0, 8).map(anime => (
+                            <img key={anime.id} src={anime.imageUrl} alt={`Anime photo ${anime.id + 1}`}/>
+                        ))
+                    )}
+                </div>
+                <div className={styles["userHobbyBoxTitle"]}>
+                    <strong>Anime</strong>
+                </div>
+            </div>
+
+            <div onClick={() => setExpandedHobby('manga')} className={`${styles["userHobbyBox"]} ${styles["bookImagesRes"]}`}>
+                <div className={styles["userHobbyBoxImages"]}>
+                    {mangaData && (
+                        mangaData.slice(0, 8).map(manga => (
+                            <img key={manga.id} src={manga.imageUrl} alt={`Manga photo ${manga.id + 1}`}/>
+                        ))
+                    )}
+                </div>
+                <div className={styles["userHobbyBoxTitle"]}>
+                    <strong>Manga</strong>
+                </div>
+            </div>
+
         </div>
         </>
         )}
