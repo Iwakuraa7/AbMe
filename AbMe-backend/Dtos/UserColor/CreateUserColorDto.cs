8952ecfbf985683151d61a01212d742af9e73d46
Erlan_Esengeldiev <ee12407@auca.kg>
2024-11-05 19:57:35 +0600

    Create DTOs for userColor model

---
+++ AbMe-backend/Dtos/UserColor/CreateUserColorDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbMe_backend.Dtos.UserColor
+{
+    public class CreateUserColorDto
+    {
+        public string FirstColor { get; set; }
+        public string SecondColor { get; set; }
+    }
+}
---
+++ AbMe-backend/Dtos/UserColor/UserColorDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbMe_backend.Dtos.UserColor
+{
+    public class UserColorDto
+    {
+        public int Id { get; set; }
+        public string FirstColor { get; set; }
+        public string SecondColor { get; set; }        
+    }
+}
