3510196476c722ba7fcec374a7460e14fc2ae8ea
Erlan_Esengeldiev <ee12407@auca.kg>
2024-11-15 21:09:41 +0600

    Add API logic

--- AbMe-frontend/react-frontend/pages/SearchMoviePage.jsx
+++ AbMe-frontend/react-frontend/pages/SearchMoviePage.jsx
@@ -1,20 +1,21 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { useSearchParams } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import styles from "../styles/SearchPage.module.css";
-import keys from "../keys.js"
+import { UserContext } from "../src/contexts/UserContext.jsx";
 
 export default function SearchMoviePage() {
     const [searchInput, setSearchInput] = useState(null);
     const [mediaData, setMediaData] = useState(null);
     const [currentMedia, setCurrentMedia] = useState(null);
+    const {READ_ACCESS_TOKEN} = useContext(UserContext);
 
     async function handleSearch() {
         var response = await fetch(`https://api.themoviedb.org/3/search/multi?query=${searchInput}&include_adult=false&language=en-US&page=1$include_images=true`, {
             method: "GET",
             headers: {
                 accept: "application/json",
-                Authorization: "Bearer " + keys.READ_ACCESS_TOKEN
+                Authorization: "Bearer " + READ_ACCESS_TOKEN
             }
         })
         
@@ -25,8 +26,25 @@ export default function SearchMoviePage() {
         setMediaData(data.results);
     }
 
-    async function addMediaData(media) {
+    async function addMediaData() {
+        var response = await fetch("http://localhost:5078/api/movie/create", {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem('token'),
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                Title: currentMedia.name ? currentMedia.name : currentMedia.title,
+                ImageUrl: `https://image.tmdb.org/t/p/original${currentMedia.poster_path}`
+            })
+        })
 
+        var data = await response.json();
+
+        if(data.succeeded)
+            console.log("Successfully created new media data");
+        else
+            console.log(data.message);
     }
 
     return (
@@ -55,7 +73,7 @@ export default function SearchMoviePage() {
                             <img src={mediaImage} style={{width: "300px", height: "450px"}}/><br/>
                             <h3>{media.name ? media.name : media.title}</h3>
                             {currentMedia === media && (
-                                <button onClick={() => addMediaData(currentMangaData)}>Add</button>
+                                <button onClick={() => addMediaData(currentMedia)}>Add</button>
                             )}
                         </div>
                     )
