69eea8ca878c83c94b713198c8c2dd72ecd4cef2
Erlan_Esengeldiev <ee12407@auca.kg>
2024-11-17 17:09:04 +0600

    Minor change to the text

--- AbMe-frontend/react-frontend/components/AboutInfo.jsx
+++ AbMe-frontend/react-frontend/components/AboutInfo.jsx
@@ -6,7 +6,7 @@ export default function AboutInfo() {
             <p>
                 AbMe - a web application that allows people to get to know each other more by viewing their profiles.
                 Each user will have their own profile in which they can share what they love. By term "what they love"
-                I mean they love to do, listen, watch, what are their beliefs, patterns, etc. 
+                We mean they love to do, listen, watch, what are their beliefs, patterns, etc. 
                 All this stuff can be shared in their profile. This app is like reposts in tiktok but much more advanced.
                 It would be an aesthetic web app in which people share their souls.
                 And only accounts that follow each other would have access to their profiles.
